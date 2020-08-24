0947572db300cb95e6d9805bec789db2cf167bc1
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-24 21:43:44 +0500

    upd

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn search(range: Range<f64>, delta: f64, eps: f64, f: impl Fn(f64) -> f64) -> f6
     while eps <= end - start {
         let x1 = 0.5 * (start + end - delta);
         let x2 = 0.5 * (start + end + delta);
-        if f(x1) <= f(x2) {
+        if f(x1) < f(x2) {
             end = x2;
         } else {
             start = x1;
