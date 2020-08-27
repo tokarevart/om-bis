6f76bfea4ce0617f7592cc26acd18bd0496e8c85
Tokarev Artyom <tokarev28.art@gmail.com>
2020-08-28 00:35:18 +0500

    upd

--- src/lib.rs
+++ src/lib.rs
@@ -17,3 +17,21 @@ pub fn search(range: Range<f64>, delta: f64, eps: f64, f: impl Fn(f64) -> f64) -
 
     0.5 * (start + end)
 }
+
+pub fn search_with_n(range: Range<f64>, delta: f64, n: usize, f: impl Fn(f64) -> f64) -> f64 {
+    assert!(delta > 0.0);
+    assert!(n > 0);
+
+    let Range{ mut start, mut end } = range;
+    for _ in 0..n {
+        let x1 = 0.5 * (start + end - delta);
+        let x2 = 0.5 * (start + end + delta);
+        if f(x1) < f(x2) {
+            end = x2;
+        } else {
+            start = x1;
+        }
+    }
+
+    0.5 * (start + end)
+}
